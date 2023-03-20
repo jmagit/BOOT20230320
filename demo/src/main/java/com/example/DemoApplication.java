e726c5fe5dc77f8c37644e5a5dd530e6d6fc8113
Javier <jmartin@grupoloyal.com>
2023-03-20 12:10:47 +0100

    Proyecto inicial

---
+++ demo/src/main/java/com/example/DemoApplication.java
@@ -0,0 +1,19 @@
+package com.example;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Adios mundo");
+	}
+
+}
