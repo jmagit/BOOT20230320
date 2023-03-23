9775edf7af94d0fb8429868d39f4a811dc2ce0f9
Javier <jmartin@grupoloyal.com>
2023-03-23 11:29:57 +0100

    RepetitionInfo

--- demo/src/test/java/com/example/ejemplos/PersonaTest.java
+++ demo/src/test/java/com/example/ejemplos/PersonaTest.java
@@ -3,6 +3,8 @@ package com.example.ejemplos;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.RepetitionInfo;
 import org.junit.jupiter.api.Test;
 
 class PersonaTest {
@@ -20,6 +22,21 @@ class PersonaTest {
 		assertAll("Validar propiedades",
 			() -> assertEquals(1, p.getId(), "id"),
 			() -> assertEquals("Pepito", p.getNombre(), "getNombre"),
+			() -> assertEquals("Grillo", p.getApellidos(), "getApellidos")
+		);
+	}
+	@RepeatedTest(value = 5, name = "{displayName} {currentRepetition}/{totalRepetitions}")
+	void repeatedTest(RepetitionInfo repetitionInfo) {
+		var p = Persona.builder()
+				.id(repetitionInfo.getCurrentRepetition())
+				.nombre("Pepito" + (repetitionInfo.getCurrentRepetition() % 3 == 0 ? "" : repetitionInfo.getCurrentRepetition()))
+				.apellidos("Grillo").build();
+//		p = null;
+//		assertNotNull(p);
+		assertTrue(p instanceof Persona, "No es instancia de persona");
+		assertAll("Validar propiedades",
+			() -> assertEquals(repetitionInfo.getCurrentRepetition(), p.getId(), "id"),
+			() -> assertEquals("Pepito" + repetitionInfo.getCurrentRepetition(), p.getNombre(), "getNombre"),
 			() -> assertEquals("Grillo", p.getApellidos(), "getApellidos")
 		);
 	}
