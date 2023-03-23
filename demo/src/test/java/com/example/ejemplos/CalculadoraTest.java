a8b545f49874b8eebce4c618070d17752eef12d6
Javier <jmartin@grupoloyal.com>
2023-03-23 09:26:13 +0100

    Refactor

--- demo/src/test/java/com/example/ejemplos/CalculadoraTest.java
+++ demo/src/test/java/com/example/ejemplos/CalculadoraTest.java
@@ -3,66 +3,95 @@ package com.example.ejemplos;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 class CalculadoraTest {
+	Calculadora calc;
 
 	@BeforeEach
 	void setUp() throws Exception {
+		calc = new Calculadora();
 	}
 
-	@Test
-	void testSumaPositivoPositivo() {
-		var calc = new Calculadora();
-		
-		var rslt = calc.suma(2, 2);
-		
-		assertEquals(4, rslt);
+	@Nested
+	class Suma {
+		@Nested
+		class OK {
+			@Test
+			void testSumaPositivoPositivo() {
+				var calc = new Calculadora();
+
+				var rslt = calc.suma(2, 2);
+
+				assertEquals(4, rslt);
+			}
+
+			@Test
+			void testSumaPositivoNegativo() {
+				var calc = new Calculadora();
+
+				var rslt = calc.suma(1, -0.9);
+
+				assertEquals(0.1, rslt);
+			}
+
+			@Test
+			void testSumaNegativoPositivo() {
+				var calc = new Calculadora();
+
+				var rslt = calc.suma(-1, 5);
+
+				assertEquals(4, rslt);
+			}
+
+			@Test
+			void testSumaNegativoNegativo() {
+				var calc = new Calculadora();
+
+				var rslt = calc.suma(-1, -5);
+
+				assertEquals(-6, rslt);
+			}
+
+			@Test
+			void testSumaDecimales() {
+				var calc = new Calculadora();
+
+				var rslt = calc.suma(0.1, 0.2);
+
+				assertEquals(0.3, rslt);
+			}
+
+		}
+
+		@Nested
+		class KO {
+
+		}
+
 	}
 
-	@Test
-	void testSumaPositivoNegativo() {
-		var calc = new Calculadora();
-		
-		var rslt = calc.suma(1, -0.9);
-		
-		assertEquals(0.1, rslt);
-	}
+	@Nested
+	class Divide {
+		@Nested
+		class OK {
+			@Test
+			void testDividirPorCero() {
+				var calc = new Calculadora();
 
-	@Test
-	void testSumaNegativoPositivo() {
-		var calc = new Calculadora();
-		
-		var rslt = calc.suma(-1, 5);
-		
-		assertEquals(4, rslt);
-	}
+				var rslt = calc.divide(1, 0.0);
 
-	@Test
-	void testSumaNegativoNegativo() {
-		var calc = new Calculadora();
-		
-		var rslt = calc.suma(-1, -5);
-		
-		assertEquals(-6, rslt);
-	}
+				assertEquals(Double.POSITIVE_INFINITY, rslt);
+			}
 
-	@Test
-	void testSumaDecimale() {
-		var calc = new Calculadora();
-		
-		var rslt = calc.suma(0.1, 0.2);
-		
-		assertEquals(0.3, rslt);
-	}
+		}
 
-	@Test
-	void testDividirPorCero() {
-		var calc = new Calculadora();
-		
-		var rslt = calc.divide(1, 0.0);
-		
-		assertEquals(Double.POSITIVE_INFINITY, rslt);
+		@Nested
+		class KO {
+
+		}
+
 	}
 
 }
