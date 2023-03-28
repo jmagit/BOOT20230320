532c8b828c886269cd6d64a6de9d75b1b728cfdb
Javier <jmartin@grupoloyal.com>
2023-03-28 08:49:59 +0200

    jdbcTemplate

--- demo/src/main/java/com/example/DemoApplication.java
+++ demo/src/main/java/com/example/DemoApplication.java
@@ -1,5 +1,8 @@
 package com.example;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
@@ -7,6 +10,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Lazy;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.ResultSetExtractor;
+import org.springframework.jdbc.core.RowMapper;
 
 import com.example.ioc.Rango;
 import com.example.ioc.StringRepository;
@@ -15,6 +21,9 @@ import com.example.ioc.StringRepositoryMockImpl;
 import com.example.ioc.StringService;
 import com.example.ioc.StringServiceImpl;
 import com.example.ioc.UnaTonteria;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
 
 @SpringBootApplication
 public class DemoApplication implements CommandLineRunner {
@@ -40,12 +49,27 @@ public class DemoApplication implements CommandLineRunner {
 	
 	@Autowired(required = false)
 	UnaTonteria tonteria;
-	
+
+	@Autowired
+	JdbcTemplate jdbcTemplate;
+
+	@Data @AllArgsConstructor
+	class Actor {
+		private int id;
+		private String first_name, last_name;
+	}
+	class ActorRowMapper implements RowMapper<Actor> {
+	      @Override
+	      public Actor mapRow(ResultSet rs, int rowNum) throws SQLException {
+	            return new Actor(rs.getInt("actor_id"), rs.getString("last_name"), rs.getString("first_name"));
+	      }
+	}
+
 	@Override
 	public void run(String... args) throws Exception {
 		System.out.println("Aplicación arrancada");
 		
-		StringRepository dao = new StringRepositoryImpl();
+//		StringRepository dao = new StringRepositoryImpl();
 //		dao = new StringRepositoryMockImpl();
 //		var srv = new StringServiceImpl(dao);
 //		System.out.println(srv.get(1));
@@ -56,9 +80,20 @@ public class DemoApplication implements CommandLineRunner {
 //		srvLocal.add("Este es el local");
 //		System.out.println(rango.toString());
 //		System.out.println(rango.getMin() + rango.getMax());
-		System.out.println(tonteria != null ? tonteria.dimeAlgo() : "Tonteria nula");
-		System.out.println(config);
-		srv.add("algo");
+//		System.out.println(tonteria != null ? tonteria.dimeAlgo() : "Tonteria nula");
+//		System.out.println(config);
+//		srv.add("algo");
+		var lst = jdbcTemplate.query("""
+				SELECT actor_id, first_name, last_name
+				from actor
+				""", new ActorRowMapper()
+				);
+		//lst.forEach(System.out::println);
+		jdbcTemplate.queryForList("""
+				SELECT concat(first_name, ' ', last_name)
+				from actor
+				""", String.class).forEach(System.out::println);
+
 	}
 
 }
