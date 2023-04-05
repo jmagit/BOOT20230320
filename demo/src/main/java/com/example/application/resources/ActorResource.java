f43e0dc3aaaf1868c9e6363c4221a8a3cd7915f0
Javier <jmartin@grupoloyal.com>
2023-04-05 14:19:39 +0200

    Esquleto

---
+++ demo/src/main/java/com/example/application/resources/ActorResource.java
@@ -0,0 +1,63 @@
+package com.example.application.resources;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.example.domains.contracts.services.ActorService;
+import com.example.domains.entities.dtos.ActorDTO;
+import com.example.exceptions.BadRequestException;
+import com.example.exceptions.NotFoundException;
+
+import jakarta.validation.Valid;
+
+import org.springframework.http.HttpStatus;
+
+@RestController
+@RequestMapping(path = { "/api/actores/v1", "/api/actors" })
+public class ActorResource {
+	private ActorService srv;
+
+	@GetMapping
+	public List<ActorDTO> getAll() {
+		// …
+	}
+
+	@GetMapping(path = "/{id}")
+	public ActorDTO getOne(@PathVariable int id) throws NotFoundException {
+		// …
+	}
+	@PostMapping
+	public ResponseEntity<Object> create(@Valid @RequestBody ActorDTO item) throws BadRequestException {
+		// …
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+			.buildAndExpand(newItem.getActorId()).toUri();
+		return ResponseEntity.created(location).build();
+
+	}
+
+	@PutMapping("/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void update(@PathVariable int id, @Valid @RequestBody ActorDTO item) throws BadRequestException, NotFoundException {
+		// …
+	}
+
+	@DeleteMapping("/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void delete(@PathVariable int id) {
+		// ..
+	}
+
+}
